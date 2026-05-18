a194e04d74f8692348dd557e58326324c048a1fd
Ychnightder <pierreychnightder561@gmail.com>
2026-05-18 16:16:45 +0200

    Remove PostGIS from installed apps in settings

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    'django.contrib.gis',
+    # 'django.contrib.gis',
     'bridges',
     'corsheaders',
 
